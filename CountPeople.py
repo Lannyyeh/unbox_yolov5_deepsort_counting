3af62d6774b50fb664b0e7f7ff0b0d6b3f2d92f4
Lannyyeh <lannyyeh11@gmail.com>
2023-12-06 20:32:28 +0800

    add description

--- CountPeople.py
+++ CountPeople.py
@@ -8,41 +8,45 @@ if __name__ == '__main__':
 
     # 根据视频尺寸，填充一个polygon，供撞线计算使用
     mask_image_temp = np.zeros((1080, 1920), dtype=np.uint8)
-    
 
     # 初始化第一个撞线polygon，上行线
-    list_pts_blue = [[833,79],[887,135],[1134,183],[1383,172],[1852,275],
-                     [1841,343],[1358,230],[1089,236],[803,179],[765,87]]
-                      
+    list_pts_blue = [[833, 79], [887, 135], [1134, 183], [1383, 172], [1852, 275],
+                     [1841, 343], [1358, 230], [1089, 236], [803, 179], [765, 87]]
+
     ndarray_pts_blue = np.array(list_pts_blue, np.int32)
-    polygon_blue_value_1 = cv2.fillPoly(mask_image_temp, [ndarray_pts_blue], color=1)
-    polygon_blue_value_1 = polygon_blue_value_1[:, :, np.newaxis] 
+    polygon_blue_value_1 = cv2.fillPoly(
+        mask_image_temp, [ndarray_pts_blue], color=1)
+    polygon_blue_value_1 = polygon_blue_value_1[:, :, np.newaxis]
     # comment by lanny
     # cv2.fillPoly可以就理解成塗色，會把上面list中的點都連起來；左上角是00點，x往右增加，y往下增加。
 
     # 填充第二个polygon，下行线
     mask_image_temp = np.zeros((1080, 1920), dtype=np.uint8)
-    list_pts_yellow = [[75,316],[96,490],[527,730],[1024,766],[1770,914],
-                       [1750,999],[987,818],[494,798],[46,522],[22,330]]
-                    
+    list_pts_yellow = [[75, 316], [96, 490], [527, 730], [1024, 766], [1770, 914],
+                       [1750, 999], [987, 818], [494, 798], [46, 522], [22, 330]]
+
     ndarray_pts_yellow = np.array(list_pts_yellow, np.int32)
-    polygon_yellow_value_2 = cv2.fillPoly(mask_image_temp, [ndarray_pts_yellow], color=2)
+    polygon_yellow_value_2 = cv2.fillPoly(
+        mask_image_temp, [ndarray_pts_yellow], color=2)
     polygon_yellow_value_2 = polygon_yellow_value_2[:, :, np.newaxis]
 
     # 填充第三个polygon，中間區域
     mask_image_temp = np.zeros((1080, 1920), dtype=np.uint8)
-    list_pts_gray=[[85,322],[118,493],[546,734],[1040,767],[1803,921],
-                   [1894,524],[1872,368],[1388,244],[1100,253],[822,193],[778,101]]
-    
+    list_pts_gray = [[85, 322], [118, 493], [546, 734], [1040, 767], [1803, 921],
+                     [1894, 524], [1872, 368], [1388, 244], [1100, 253], [822, 193], [778, 101]]
+
     ndarray_pts_gray = np.array(list_pts_gray, np.int32)
-    polygon_gray_value_3 = cv2.fillPoly(mask_image_temp, [ndarray_pts_gray],color=3)
-    polygon_gray_value_3=polygon_gray_value_3[:,:,np.newaxis]
+    polygon_gray_value_3 = cv2.fillPoly(
+        mask_image_temp, [ndarray_pts_gray], color=3)
+    polygon_gray_value_3 = polygon_gray_value_3[:, :, np.newaxis]
 
     # 撞线检测用mask，包含3个polygon，（值范围 0、1、2、3），供撞线计算使用
-    polygon_mask_blue_and_yellow = polygon_blue_value_1 + polygon_yellow_value_2+ polygon_gray_value_3
+    polygon_mask_blue_and_yellow = polygon_blue_value_1 + \
+        polygon_yellow_value_2 + polygon_gray_value_3
 
     # 缩小尺寸，1920x1080->960x540
-    polygon_mask_blue_and_yellow = cv2.resize(polygon_mask_blue_and_yellow, (960, 540))
+    polygon_mask_blue_and_yellow = cv2.resize(
+        polygon_mask_blue_and_yellow, (960, 540))
 
     # 蓝 色盘 b,g,r
     blue_color_plate = [255, 0, 0]
@@ -52,12 +56,12 @@ if __name__ == '__main__':
     # 黄 色盘
     yellow_color_plate = [0, 255, 255]
     # 黄 polygon图片
-    yellow_image = np.array(polygon_yellow_value_2 * yellow_color_plate, np.uint8)
+    yellow_image = np.array(polygon_yellow_value_2 *
+                            yellow_color_plate, np.uint8)
 
     # 灰色圖片
     gray_color_plate = [102, 102, 153]
-    gray_image = np.array(polygon_gray_value_3 * gray_color_plate,np.uint8)
-
+    gray_image = np.array(polygon_gray_value_3 * gray_color_plate, np.uint8)
 
     # 彩色图片（值范围 0-255）
     color_polygons_image = blue_image + yellow_image + gray_image
@@ -103,7 +107,8 @@ if __name__ == '__main__':
 
             # 画框
             # 撞线检测点，(x1，y1)，y方向偏移比例 0.0~1.0
-            output_image_frame = tracker.draw_bboxes(im, list_bboxs, line_thickness=None)
+            output_image_frame = tracker.draw_bboxes(
+                im, list_bboxs, line_thickness=None)
             pass
         else:
             # 如果画面中 没有bbox
@@ -129,12 +134,12 @@ if __name__ == '__main__':
                     # 如果撞 蓝polygon
                     if track_id not in list_overlapping_blue_polygon:
                         list_overlapping_blue_polygon.append(track_id)
-                        print(f'All blue:{list_overlapping_blue_polygon}')
-                    pass # 如果已经撞过蓝色的线，就什么都不做，表示还没走出蓝色线范围
+                    pass  # 如果已经撞过蓝色的线，就什么都不做，表示还没走出蓝色线范围
 
                     if track_id in list_overlapping_gray_polygon:
                         leave_count += 1
-                        print(f'类别: {label} | id: {track_id} | 从上方离开区域 | 离开人数: {leave_count} | 图中人物：{list_overlapping_gray_polygon}')
+                        print(
+                            f'类别: {label} | id: {track_id} | 从上方离开区域 | 离开人数: {leave_count} | 图中人物：{list_overlapping_gray_polygon}')
                         list_overlapping_gray_polygon.remove(track_id)
 
                         pass
@@ -147,38 +152,41 @@ if __name__ == '__main__':
                     if track_id not in list_overlapping_yellow_polygon:
                         list_overlapping_yellow_polygon.append(track_id)
                     pass
-                    
+
                     if track_id in list_overlapping_gray_polygon:
-                        
+
                         leave_count += 1
-                        print(f'类别: {label} | id: {track_id} | 从下方离开区域 | 离开人数: {leave_count} | 图中人物：{list_overlapping_gray_polygon}')
+                        print(
+                            f'类别: {label} | id: {track_id} | 从下方离开区域 | 离开人数: {leave_count} | 图中人物：{list_overlapping_gray_polygon}')
                         list_overlapping_gray_polygon.remove(track_id)
                         pass
                     else:
                         # 无此 track_id，不做其他操作
                         pass
                     pass
-                
+
                 elif polygon_mask_blue_and_yellow[y, x] == 3:
 
                     if track_id not in list_overlapping_gray_polygon:
                         list_overlapping_gray_polygon.append(track_id)
                     pass
-                
+
                     if track_id in list_overlapping_blue_polygon:
-                        enter_count+=1
-                        print(f'类别: {label} | id: {track_id} | 从上方进入区域 | 进入人数: {enter_count} | 图中人物：{list_overlapping_gray_polygon}')
+                        enter_count += 1
+                        print(
+                            f'类别: {label} | id: {track_id} | 从上方进入区域 | 进入人数: {enter_count} | 图中人物：{list_overlapping_gray_polygon}')
                         list_overlapping_blue_polygon.remove(track_id)
                     elif track_id in list_overlapping_yellow_polygon:
-                        enter_count+=1
-                        print(f'类别: {label} | id: {track_id} | 从下方进入区域 | 进入人数: {enter_count} | 图中人物：{list_overlapping_gray_polygon}')
+                        enter_count += 1
+                        print(
+                            f'类别: {label} | id: {track_id} | 从下方进入区域 | 进入人数: {enter_count} | 图中人物：{list_overlapping_gray_polygon}')
                         list_overlapping_yellow_polygon.remove(track_id)
                     else:
                         # 表示人物未曾经过蓝色或黄色区域，突然出现在中间灰色区域中
                         # 有两种可能：一种是原本就在的人物，一种是跟踪失败的人物
                         # 先不作处理
                         pass
-                            
+
                 else:
                     pass
                 pass
@@ -186,7 +194,8 @@ if __name__ == '__main__':
             pass
 
             # ----------------------清除无用id----------------------
-            list_overlapping_all = list_overlapping_yellow_polygon + list_overlapping_blue_polygon
+            list_overlapping_all = list_overlapping_yellow_polygon + \
+                list_overlapping_blue_polygon
             for id1 in list_overlapping_all:
                 is_found = False
                 for _, _, _, _, _, bbox_id in list_bboxs:
--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# 碩一課程Cyber-Physics System 的專案
+# 碩一課程 Cyber-Physics System 的專案
 
 ## 簡介
 
 - 功能可能比較簡陋。
 - 我們小組關於物聯網的大作業，是打算做一個攝像頭人物檢測的專案。
-- 攝像頭那邊應該要是一個簡單的IOT設備，應該要是無法進行太複雜的運算，只能將獲取到的信息傳輸到計算中心進行處理，再接收一個結果，這樣簡單的設備；但是，由於我們沒有這種嵌入式設備，所以目前client和server用的都是普通pc，對於client來說是性能是冗餘的。
+- 攝像頭那邊應該要是一個簡單的嵌入式設備，應該要是無法進行太複雜的運算，只能將獲取到的信息傳輸到計算中心進行處理，再接收一個結果這樣簡單的設備；但是我們沒有這種設備，所以目前client和server用的都是普通pc，對於client來說是性能應該是算是冗餘的。
 - 總之，傳輸這邊最後交由到另外兩位大佬負責，我負責的部分是server端的人物識別和追蹤。輸入會是根據rtmp協議獲取的圖像，code的功能只要從 `cv2.VideoCapture()` 開始就可以了。模型使用的是fork過來的原版，只有在具體功能上做一些修改，以及可能因為版本較舊造成的error進行一點點處理，改的不多，基本能直接跑通。
 
 ## 環境配置和流程
 
-1. 首先需要裝CUDA和Cudnn，以及pytorch，我這邊最後使用的cuda 12.0和pytorch 11.8
-2. 環境配置使用的是conda，可以直接使用[配置文件](./environment.yml)
+1. 首先需要裝CUDA和Cudnn，以及pytorch，我這邊最後使用的cuda 12.0和pytorch 11.8，主要參考的是[這個教學](https://zhuanlan.zhihu.com/p/646484518)
+2. 環境配置使用的是conda，可以直接使用[配置文件](./environment.yml)；使用原本作者給的[requirements](./requirements.txt)不太順利。
 
     ``` bash
     conda env create -f environment.yml --name new_env_name
@@ -22,8 +22,8 @@
     python main.py
     ```
 
-4. 基於原本程式寫的檢測人物進入和離開一個區域的統計。
-    - 測試資源目前還是原本的行人影片
+4. 參考原本程式寫的檢測人物進入和離開一個區域的統計。
+    - 測試使用的資源目前還是原本的行人影片
 
     ``` bash
     python CountPeople.py
@@ -31,5 +31,5 @@
 
 ## 對原版的修改記錄
 
-- 原版程式中使用了一些`np.float` 和 `np.int`此類的格式，根據error信息，這種寫法已被捨棄，可以直接使用`float` 和 `int`；或者用`np.float64` 和 `np.float32`這樣的寫法。
+- 原版程式中使用了一些`np.float` 和 `np.int`這樣的寫法，根據error信息，這種寫法已被捨棄，可以直接使用`float` 和 `int`；或者用`np.float64` 和 `np.float32`。
 - 其餘的可以參照[作者原本的說明](./README_origin.md)
